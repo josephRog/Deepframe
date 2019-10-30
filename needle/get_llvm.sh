c60c03a0982050490c5cb6b53d5e5968346478f3
josephRog <jrogers234@gmail.com>
2019-10-30 11:02:20 +0100

    updated get llvm script

--- needle/get_llvm.sh
+++ needle/get_llvm.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+# Edit the following vars to be correct for your setup
+
+# Can check different LLVM versions on llvm.org
+OS_VERSION=x86_64-linux-gnu-ubuntu-16.04
+LLVM_SOURCE=http://releases.llvm.org/3.8.1/clang+llvm-3.8.1-${OS_VERSION}.tar.xz
+
+
+echo "Starting get LLVM for needle script"
+echo "OS_VERSION set as: ${OS_VERSION}"
+echo "LLVM_SOURCE set as: ${LLVM_SOURCE}"
 
 if [ ! -d "llvm-3.8" ]; then
     echo "Downloading LLVM (145M)"
-    wget http://releases.llvm.org/3.8.1/clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    wget $LLVM_SOURCE
     echo -n "Unpacking LLVM, this can take some time ... "
-    tar xf clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+    tar xf clang+llvm-3.8.1-${OS_VERSION}.tar.xz
     echo "Done"
-    mv clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04 llvm-3.8
-    mv clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz llvm-3.8
+    
+    # Move LLVM things around
+    mv clang+llvm-3.8.1-${OS_VERSION} llvm-3.8
+    mv clang+llvm-3.8.1-${OS_VERSION}.tar.xz llvm-3.8
 else
     echo "llvm-3.8 directory already exists, please remove to redownload LLVM"
 fi
